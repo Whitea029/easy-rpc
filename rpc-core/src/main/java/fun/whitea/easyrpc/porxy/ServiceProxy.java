9a992842e41673d619c7bde77959519bdc77c191
Whitea029 <1664915116@qq.com>
2024-09-27 14:19:44 +0800

    feat: refactor register to registry

--- rpc-core/src/main/java/fun/whitea/easyrpc/porxy/ServiceProxy.java
+++ rpc-core/src/main/java/fun/whitea/easyrpc/porxy/ServiceProxy.java
@@ -1,24 +1,32 @@
 package fun.whitea.easyrpc.porxy;
 
-import ch.qos.logback.core.util.CloseUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import fun.whitea.easyrpc.RpcApplication;
 import fun.whitea.easyrpc.config.RpcConfig;
+import fun.whitea.easyrpc.constant.ProtocolConstant;
 import fun.whitea.easyrpc.constant.RpcConstant;
 import fun.whitea.easyrpc.model.RpcRequest;
 import fun.whitea.easyrpc.model.RpcResponse;
+import fun.whitea.easyrpc.protocol.*;
 import fun.whitea.easyrpc.registry.RegisterFactory;
 import fun.whitea.easyrpc.registry.Registry;
 import fun.whitea.easyrpc.registry.ServiceMetaInfo;
 import fun.whitea.easyrpc.serializer.SerializeFactory;
 import fun.whitea.easyrpc.serializer.Serializer;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetSocket;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 public class ServiceProxy implements InvocationHandler {
     @Override
@@ -33,7 +41,6 @@ public class ServiceProxy implements InvocationHandler {
                 .args(args)
                 .build();
         try {
-            byte[] serialize = serializer.serialize(rpcRequest);
             Registry registry = RegisterFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
             ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
             serviceMetaInfo.setServiceName(serviceName);
@@ -42,17 +49,61 @@ public class ServiceProxy implements InvocationHandler {
             if (CollUtil.isEmpty(serviceMetaInfos)) {
                 throw new RuntimeException("No service address");
             }
-            ServiceMetaInfo selectServiceMetaInfo = serviceMetaInfos.get(0);
-            try (HttpResponse execute = HttpRequest.post(selectServiceMetaInfo.getServiceAddress())
-                    .body(serialize)
-                    .execute()) {
-                byte[] res = execute.bodyBytes();
-                RpcResponse rpcResponse = serializer.deserialize(res, RpcResponse.class);
-                return rpcResponse.getData();
-            }
-        } catch (IOException e) {
+            ServiceMetaInfo selectServiceMetaInfo = serviceMetaInfos.getFirst();
+            return sendTcpReq(selectServiceMetaInfo, rpcRequest);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private Object sendHttpReq(ServiceMetaInfo selectServiceMetaInfo, RpcRequest rpcRequest, Serializer serializer) throws IOException {
+        try (HttpResponse execute = HttpRequest.post(selectServiceMetaInfo.getServiceAddress())
+                .body(serializer.serialize(rpcRequest))
+                .execute()) {
+            byte[] res = execute.bodyBytes();
+            RpcResponse rpcResponse = serializer.deserialize(res, RpcResponse.class);
+            return rpcResponse.getData();
+        }
+    }
+
+    private Object sendTcpReq(ServiceMetaInfo selectServiceMetaInfo, RpcRequest rpcRequest) throws Exception {
+        Vertx vertx = Vertx.vertx();
+        NetClient netClient = vertx.createNetClient();
+        CompletableFuture<RpcResponse> future = new CompletableFuture<>();
+        netClient.connect(selectServiceMetaInfo.getServicePort(), selectServiceMetaInfo.getServiceHost(), result -> {
+            if (result.succeeded()) {
+                System.out.println("Connect to Tcp Server");
+                NetSocket socket = result.result();
+                ProtocolMessage<RpcRequest> rpcRequestProtocolMessage = new ProtocolMessage<>();
+                ProtocolMessage.Header header = new ProtocolMessage.Header();
+                header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+                header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+                header.setSerializer(Byte.parseByte(Objects.requireNonNull(ProtocolMessageSerializerEnum.fromVal(RpcApplication.getConfig().getSerializer())).getVal()));
+                header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+                header.setRequestId(IdUtil.getSnowflakeNextId());
+                rpcRequestProtocolMessage.setHeader(header);
+                rpcRequestProtocolMessage.setBody(rpcRequest);
+                try {
+                    Buffer encode = ProtocolMessageEncoder.encode(rpcRequestProtocolMessage);
+                    socket.write(encode);
+                } catch (Exception e) {
+                    throw new RuntimeException("Protocol message encoding error", e);
+                }
+                socket.handler(buffer -> {
+                    try {
+                        ProtocolMessage<RpcResponse> responseProtocolMessage = (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
+                        future.complete(responseProtocolMessage.getBody());
+                    } catch (Exception e) {
+                        throw new RuntimeException("Protocol message encoding error", e);
+                    }
+                });
+            } else {
+                System.out.println("Failed to connect to TCP server");
+            }
+        });
+        RpcResponse rpcResponse = future.get();
+        netClient.close();
+        return rpcResponse.getData();
     }
 }
---
+++ rpc-core/src/main/java/fun/whitea/easyrpc/server/tcp/TcpServerHandler.java
@@ -0,0 +1,53 @@
+package fun.whitea.easyrpc.server.tcp;
+
+import fun.whitea.easyrpc.model.RpcRequest;
+import fun.whitea.easyrpc.model.RpcResponse;
+import fun.whitea.easyrpc.protocol.ProtocolMessage;
+import fun.whitea.easyrpc.protocol.ProtocolMessageDecoder;
+import fun.whitea.easyrpc.protocol.ProtocolMessageEncoder;
+import fun.whitea.easyrpc.protocol.ProtocolMessageTypeEnum;
+import fun.whitea.easyrpc.registry.LocalRegister;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetSocket;
+
+import java.lang.reflect.Method;
+
+public class TcpServerHandler implements Handler<NetSocket> {
+
+    @Override
+    public void handle(NetSocket socket) {
+        socket.handler(buffer -> {
+            ProtocolMessage<RpcRequest> repMessage;
+            try {
+                repMessage = (ProtocolMessage<RpcRequest>) ProtocolMessageDecoder.decode(buffer);
+            } catch (Exception e) {
+                throw new RuntimeException("protocol message decode error", e);
+            }
+            RpcRequest rpcReq = repMessage.getBody();
+            RpcResponse rpcResp = new RpcResponse();
+            try {
+                Class<?> implClass = LocalRegister.get(rpcReq.getServiceName());
+                Method method = implClass.getMethod(rpcReq.getMethodName(), rpcReq.getParameterTypes());
+                Object res = method.invoke(implClass.newInstance(), rpcReq.getArgs());
+                rpcResp.setData(res);
+                rpcResp.setType(method.getReturnType());
+                rpcResp.setMessage("OK");
+            } catch (Exception e) {
+                e.printStackTrace();
+                rpcResp.setMessage(e.getMessage());
+                rpcResp.setException(e);
+            }
+            ProtocolMessage.Header header = repMessage.getHeader();
+            header.setType((byte) ProtocolMessageTypeEnum.RESPONSE.getKey());
+            ProtocolMessage<RpcResponse> respMessage = new ProtocolMessage<>(header, rpcResp);
+            try {
+                Buffer encode = ProtocolMessageEncoder.encode(respMessage);
+                socket.write(encode);
+            } catch (Exception e) {
+                throw new RuntimeException("protocol message encode error", e);
+            }
+        });
+    }
+
+}
