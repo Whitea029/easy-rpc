ca3a3e5047df0a6f89ee925ae66844375ba38843
Whitea029 <1664915116@qq.com>
2024-09-29 23:16:00 +0800

    test: loadbalancer test

---
+++ rpc-core/src/main/java/fun/whitea/easyrpc/test/LoadBalancerTest.java
@@ -0,0 +1,43 @@
+package fun.whitea.easyrpc.test;
+
+import fun.whitea.easyrpc.loadbalancer.ConsistentHashLoadBalancer;
+import fun.whitea.easyrpc.loadbalancer.LoadBalancer;
+import fun.whitea.easyrpc.registry.ServiceMetaInfo;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LoadBalancerTest {
+
+    final LoadBalancer loadBalancer = new ConsistentHashLoadBalancer();
+
+    @Test
+    public void select() {
+        Map<String, Object> requestParams = new HashMap<>();
+        requestParams.put("methodName", "apple");
+        ServiceMetaInfo serviceMetaInfo1 = new ServiceMetaInfo();
+        serviceMetaInfo1.setServiceName("myService");
+        serviceMetaInfo1.setServiceVersion("1.0");
+        serviceMetaInfo1.setServiceHost("localhost");
+        serviceMetaInfo1.setServicePort(1234);
+        ServiceMetaInfo serviceMetaInfo2 = new ServiceMetaInfo();
+        serviceMetaInfo2.setServiceName("myService");
+        serviceMetaInfo2.setServiceVersion("1.0");
+        serviceMetaInfo2.setServiceHost("yupi.icu");
+        serviceMetaInfo2.setServicePort(80);
+        List<ServiceMetaInfo> serviceMetaInfoList = Arrays.asList(serviceMetaInfo1, serviceMetaInfo2);
+        ServiceMetaInfo serviceMetaInfo = loadBalancer.select(requestParams, serviceMetaInfoList);
+        System.out.println(serviceMetaInfo);
+        Assert.assertNotNull(serviceMetaInfo);
+        serviceMetaInfo = loadBalancer.select(requestParams, serviceMetaInfoList);
+        System.out.println(serviceMetaInfo);
+        Assert.assertNotNull(serviceMetaInfo);
+        serviceMetaInfo = loadBalancer.select(requestParams, serviceMetaInfoList);
+        System.out.println(serviceMetaInfo);
+        Assert.assertNotNull(serviceMetaInfo);
+    }
+}
